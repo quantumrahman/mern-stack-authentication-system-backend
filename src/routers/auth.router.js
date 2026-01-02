a34b451502d7691a089e17844e35874e6cdb9dbd
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-01-02 18:58:00 +0600

    add sign-up controller in auth.router.js file

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,10 +1,16 @@
 // import module ------------------------------------------->
 import express from 'express';
+import { signUpController } from '../controllers/auth.controllers.js';
+import validatorSchema from '../validators/signup.validator.schema.js';
+import validationMiddleware from '../middlewares/validator.middleware.js';
 
 // express router ------------------------------------------>
 const router = express.Router();
 
 // routes -------------------------------------------------->
+router
+    .route('/sign-up')
+    .post(validationMiddleware(validatorSchema), signUpController);
 
 // export module ------------------------------------------->
 export default router;
