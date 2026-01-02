2f118f5c904f766b244c428c4d43bccaf8c0c46e
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-01-02 18:32:09 +0600

    implement app error constructor in app.error.js file

---
+++ src/utils/constants/app.error.js
@@ -0,0 +1,24 @@
+// class-constructor --------------------------------------->
+class AppError extends Error {
+    constructor(
+        message,
+        {
+            success = false,
+            status = 500,
+            code = 'INTERNAL_SERVER_ERROR',
+            details = null,
+        }
+    ) {
+        super(message);
+
+        this.success = success;
+        this.status = status;
+        this.code = code;
+        this.details = details;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+// export module ------------------------------------------->
+export default AppError;
