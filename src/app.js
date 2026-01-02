7e70b3dc59bcc000b635c40160812854359f187e
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-01-02 18:44:06 +0600

    add error middleware in app.js file

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import errorMiddleware from './middlewares/validator.middleware.js';
 
 // express app --------------------------------------------->
 const app = express();
@@ -17,6 +18,9 @@ app.use(express.static('public'));
 // security middleware ------------------------------------->
 app.use(cors({ origin: String(process.env.CORS_ORIGIN), credentials: true }));
 app.use(cookieParser());
+
+// error middleware ---------------------------------------->
+app.use(errorMiddleware);
 
 // export module ------------------------------------------->
 export default app;
