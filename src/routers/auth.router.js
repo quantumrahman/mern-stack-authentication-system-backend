19c2117188271fb57c1313892ab45ea37da13f20
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-01-02 18:49:02 +0600

    implement express router in auth.router.js file

---
+++ src/routers/auth.router.js
@@ -0,0 +1,10 @@
+// import module ------------------------------------------->
+import express from 'express';
+
+// express router ------------------------------------------>
+const router = express.Router();
+
+// routes -------------------------------------------------->
+
+// export module ------------------------------------------->
+export default router;
