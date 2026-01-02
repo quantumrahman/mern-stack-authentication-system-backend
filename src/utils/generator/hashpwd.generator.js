18e6261ca50ab992a512c1ee1f7903434370c4b7
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-01-02 21:13:42 +0600

    implement hash password generator function in hashpwd.generator.js file

---
+++ src/utils/generator/hashpwd.generator.js
@@ -0,0 +1,35 @@
+// import module ------------------------------------------->
+import 'dotenv/config';
+import chalk from 'chalk';
+import bcrypt from 'bcrypt';
+
+// generator function -------------------------------------->
+const hashPwdGenerator = async (pwd) => {
+    try {
+        if (typeof pwd !== 'string') {
+            throw new Error('Password must a string format!');
+        }
+
+        const normalizedPwd = pwd.trim();
+
+        if (normalizedPwd === '') {
+            throw new Error('Password must not be empty!');
+        }
+
+        const saltRounds = Number(process.env.BCRYPT_SALT_ROUNDS);
+
+        if (!Number.isInteger(saltRounds) || saltRounds < 1) {
+            saltRounds = 10;
+        }
+
+        let hashedPwd = await bcrypt.hash(normalizedPwd, saltRounds);
+
+        return hashedPwd;
+    } catch (error) {
+        console.log(chalk.yellow('Password cannot hashing!'), error.message);
+        throw error;
+    }
+};
+
+// export module ------------------------------------------->
+export default hashPwdGenerator;
