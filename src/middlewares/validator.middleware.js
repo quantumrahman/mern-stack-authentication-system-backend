80332aa5d823dfb45f9013faedf4c0ad72cdb170
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-01-02 18:42:03 +0600

    implement validation middleware in validator.middleware.js file

---
+++ src/middlewares/validator.middleware.js
@@ -0,0 +1,31 @@
+// import module ------------------------------------------->
+import AppError from '../utils/constants/app.error.js';
+
+// validator middleware ------------------------------------>
+const validatorMiddleware = (schema) => async (req, res, next) => {
+    const result = await schema.safeParseAsync(req.body);
+
+    if (!result.success) {
+        const { fieldErrors } = result.error.flatten();
+
+        return next(
+            new AppError('Validation Error.', {
+                success: false,
+                status: 400,
+                code: 'VALIDATION_ERROR',
+                details: Object.entries(fieldErrors).map(
+                    ([field, message]) => ({
+                        field: field,
+                        message: message[0],
+                    })
+                ),
+            })
+        );
+    }
+
+    req.body = result.data;
+    return next();
+};
+
+// export module ------------------------------------------->
+export default validatorMiddleware;
