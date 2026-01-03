3ab6e0c10c3d877315c15513319155a53a0d1138
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-01-03 20:47:12 +0600

    implement hash password compare function in compare.pwd.func.js

--- src/utils/functions/compare.pwd.func.js
+++ src/utils/functions/compare.pwd.func.js
@@ -1,1 +1,28 @@
 // import module ------------------------------------------->
+import chalk from 'chalk';
+import bcrypt from 'bcrypt';
+
+// function ------------------------------------------------>
+const passwordCompare = async (inputPwd, hashPwd) => {
+    try {
+        if (typeof inputPwd !== 'string' || typeof hashPwd !== 'string') {
+            throw new Error('Password must be a string format!');
+        }
+
+        const normalizedPwd = inputPwd.trim();
+
+        if (!normalizedPwd) {
+            throw new Error('Password must not be empty.!');
+        }
+
+        const passwordCompare = await bcrypt.compare(normalizedPwd, hashPwd);
+
+        return passwordCompare;
+    } catch (error) {
+        console.log(chalk.yellow('Password cannot compare!'), error.message);
+        throw error;
+    }
+};
+
+// export module ------------------------------------------->
+export default passwordCompare;
